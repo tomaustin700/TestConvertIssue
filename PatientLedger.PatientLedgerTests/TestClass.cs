f5a7e30e2bc466171daef63cc0904282d7950058
Tom Austin <Tom.Austin@streets-heaver.com>
2019-12-23 13:12:14 +0000

    Simplified

---
+++ PatientLedger.PatientLedgerTests/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PatientLedger.PatientLedgerTests
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
